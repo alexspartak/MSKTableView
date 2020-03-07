cd2345ae8fcc53d5d372a94b8c54873f2119cbce
Alex Tishchenko <a.tishencko2017@yandex.ru>
2020-03-07 13:36:20 +0300

    Initial Commit

---
+++ MSKTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MSKTableView
+//
+//  Created by Alex Tishchenko on 3/7/20.
+//  Copyright © 2020 Alex Tishchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
